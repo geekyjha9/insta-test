8659ee1b974b567e936280678157a7fa729b1fab
Abhishek Agrawal <ashu.agrawalksj@gmail.com>
2024-07-24 22:26:54 +0530

    Updated the profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
+import AppLayout from "./pages/AppLayout";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 
@@ -14,10 +15,15 @@ function App() {
 
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/signup" element={<SignUp />}></Route>
-          <Route path="/signin" element={<SignIn />}></Route>
-          <Route path="/profile" element={<Profile />}></Route>
+          {/* Authentication Routes */}
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+
+          {/* Layout with navigation */}
+          <Route element={<AppLayout/>}>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
         </Routes>
 
       </div>
@@ -28,83 +34,6 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useState } from 'react';
-// import './App.css';
-// import { Button, Input } from '@chakra-ui/react';
-
-// function App() {
-//     const [count, setCount] = useState(0);
-//     const [isVisible, setIsVisible] = useState(true);
-//     const [inputValue, setInputValue] = useState('');
-
-//     const handleInputChange = (event) => {
-//         setInputValue(event.target.value);
-//     };
-
-//     const applyInputValue = () => {
-//         setCount(parseInt(inputValue, 10) || 0); // Only update if it's a valid number
-//     };
-
-//     return (
-//         <>
-//             <div>
-//                 <Button onClick={() => setIsVisible(!isVisible)}>
-//                     Toggle Counter Visibility
-//                 </Button>
-//                 <Input
-//                     value={inputValue}
-//                     onChange={handleInputChange}
-//                     placeholder="Set counter value"
-//                 />
-//                 <Button
-//                     onClick={applyInputValue}
-//                     className="set-counter-button"
-//                 >
-//                     Set Counter
-//                 </Button>
-//                 <Button onClick={() => setCount(0)}>Reset Counter</Button>
-//             </div>
-//             <h1>Vite + React</h1>
-//             <h2>Vite + h2 + React</h2>
-//             {isVisible && (
-//                 <div className="card">
-//                     <Button
-//                         onClick={() => setCount((count) => count + 1)}
-//                         className="chakra-button"
-//                         backgroundColor="rgb(239, 239, 239)"
-//                     >
-//                         count is {count}
-//                     </Button>
-//                 </div>
-//             )}
-//             <p className="read-the-docs">
-//                 Click on the Vite and React logos to learn more
-//             </p>
-//         </>
-//     );
-// }
-
-// export default App;
-
-
-
-
-
 
 
 
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -5,7 +5,7 @@ import ProfilePosts from '../components/Profile/ProfilePost';
 
 const Profile = () => {
   return (
-    <div className="max-w-4xl mx-auto">
+    <div className="max-w-4xl w-full lg:w-[70%] h-auto mx-auto mt-9 mb-5 pt-9">
       <ProfileHeader />
       <ProfileBio />
       <ProfilePosts />
